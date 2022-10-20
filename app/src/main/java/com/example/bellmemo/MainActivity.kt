9c8baef06bafd8635409b9929ba973326f466997
liyl <365908865@qq.com>
2022-10-21 00:29:22 +0800

    feat: add room

--- app/src/main/java/com/example/bellmemo/MainActivity.kt
+++ app/src/main/java/com/example/bellmemo/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.bellmemo
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -47,7 +46,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
