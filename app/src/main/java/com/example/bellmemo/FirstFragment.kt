ebdc1cd772e59cd8269dee032ecbb9ebe8b31339
liyl <365908865@qq.com>
2022-11-04 00:26:33 +0800

    feat: toast

--- app/src/main/java/com/example/bellmemo/FirstFragment.kt
+++ app/src/main/java/com/example/bellmemo/FirstFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import com.example.bellmemo.databinding.FragmentFirstBinding
 
 /**
@@ -27,14 +26,6 @@ class FirstFragment : Fragment() {
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
 
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
     }
 
     override fun onDestroyView() {
